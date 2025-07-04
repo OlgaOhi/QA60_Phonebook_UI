c3df2c75dee628986b8a88a418dc449eab7c87d2
OlgaOhi <ogievich1881@gmail.com>
2025-07-04 09:44:38 +0200

    add phonebook

---
+++ src/main/java/com/phonebook/data/ContactData.java
@@ -0,0 +1,11 @@
+package com.phonebook.data;
+
+public class ContactData {
+    public static final String NAME = "Karl";
+    public static final String LAST_NAME = "Adam";
+    public static final String PHONE = "1234567881";
+    public static final String EMAIL = "Karl@GMAIL.COM";
+    public static final String ADDRESS = "Karl";
+    public static final String DESCRIPTION = "Berlin";
+
+}
---
+++ src/test/java/com/phonebook/tests/LoginTests.java
@@ -0,0 +1,36 @@
+
+package com.phonebook.tests;
+
+import com.phonebook.data.UserData;
+import com.phonebook.models.User;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTests extends TestBase{
+
+    @BeforeMethod
+    public void ensurePrecondition() {
+        if (!app.getUser().isLoginLinkPresent()) {
+            app.getUser().clickOnSignOutButton();
+        }
+    }
+
+    @Test(priority = 1)
+    public void loginPositiveTest(){
+        app.getUser().clickOnLoginLink();
+        app.getUser().fillRegisterLoginForm(new User().setEmail(UserData.EMAIL).setPassword(UserData.PASSWORD));
+        app.getUser().clickOnLoginButton();
+        Assert.assertTrue(app.getUser().isSignOutButtonPresent());
+
+    }
+    @Test(priority = 2)
+    public void loginNegativeTest(){
+        app.getUser().clickOnLoginLink();
+        app.getUser().fillRegisterLoginForm(new User().setEmail("").setPassword(UserData.PASSWORD));
+        app.getUser().clickOnLoginButton();
+        Assert.assertTrue(app.getUser().isAlertDisplyed());
+
+    }
+
+}
